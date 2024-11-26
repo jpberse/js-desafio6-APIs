ddb67003635fcf54cf3fec2d63db81be6724d552
jpberse <jp.bersezio@gmail.com>
2024-11-26 15:25:46 -0300

    creacion de grafica para cada moneda en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const selectMoneda = document.querySelector('#select-moneda');
 const resultado = document.querySelector('#resultado');
 const btnConvertir = document.querySelector('#btn-convertir');
 const titulo = document.querySelector('.titulo')
-const cambioMoneda = selectMoneda.value;
+let chartInstance = null 
 
 
 async function getMonedas() {
@@ -21,11 +21,56 @@ async function getMonedas() {
 async function getChart(tipo) {
     try {
         const res = await fetch(`https://mindicador.cl/api/${tipo}`);
-        const tipoMoneda = res.json();
+        const tipoMoneda = await res.json();
         return tipoMoneda
     } catch(e) {
         console.log(e.message)
     }
+}
+
+const configChart = (monedas) => {
+    const fechaMoneda = monedas.serie.map((moneda) => moneda.fecha.slice(0,10));
+    fechaMoneda.splice(10);
+    fechaMoneda.reverse();
+    const titulo = monedas.codigo;
+    let colorLinea = '' ;
+    const valores = monedas.serie.map((moneda) => {
+        const valor = moneda.valor
+        return Number(valor);
+    })
+    
+    if(monedas.codigo == 'euro') {
+        colorLinea = 'blue'
+    } else if(monedas.codigo == 'bitcoin') {
+        colorLinea = 'orange'
+    } else {
+        colorLinea = 'red'
+    }
+
+    const config = {
+        type: 'line',
+        data: {
+            labels: fechaMoneda,
+            datasets: [{
+                label: titulo,
+                backgroundColor: colorLinea,
+                data: valores
+            }]
+        }
+    };
+    return config
+}
+
+async function renderChart(moneda) {
+    const monedas = await getChart(moneda);
+    const config = configChart(monedas);
+    const chartDom = document.querySelector('#chart-monedas');
+
+    if(chartInstance){
+        chartInstance.destroy()
+    }
+    
+    chartInstance = new Chart(chartDom, config);
 }
 
 getMonedas();
@@ -36,6 +81,8 @@ btnConvertir.addEventListener('click', async () => {
     const valorCambio = monedas[cambio].valor;
     const pesoCLP = inputCLP.value;
 
+    console.log(valorCambio)
+
     if(isNaN(pesoCLP) || pesoCLP <= 0) {
         resultado.textContent = 'Por favor, ingresa un monto valido';
         return
@@ -45,9 +92,12 @@ btnConvertir.addEventListener('click', async () => {
 
     if(cambio === 'dolar') {
         resultado.innerHTML = `$ ${result}`;
+        renderChart('dolar')
     } else if (cambio === 'euro') {
         resultado.innerHTML = `€ ${result}`;
+        renderChart('euro')
     } else if(cambio === 'bitcoin') {
         resultado.innerHTML = `₿ ${result}`;
+        renderChart('bitcoin')
     }
 })
